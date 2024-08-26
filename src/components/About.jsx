634ad812eb03262a68456ab7263a549e93561661
Jhon Esparragoza <jhonantonyesp@gmail.com>
2024-08-26 16:20:34 +0200

    Cambios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,12 +37,7 @@ const About = () => {
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+        With a passion for programming since I was young, I studied computer engineering at UNEG in Venezuela, I have done a variety of jobs and projects as a freelancer. Now I am in Italy and I am looking for new experiences and to continue improving as a programmer and developer
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
--- src/constants/index.js
+++ src/constants/index.js
@@ -54,10 +54,6 @@ const services = [
     icon: backend,
   },
   {
-    title: 'UI/UX Design',
-    icon: ux,
-  },
-  {
     title: 'Software Prototyping',
     icon: prototyping,
   },
@@ -97,10 +93,6 @@ const technologies = [
     icon: nodejs,
   },
   {
-    name: 'Rails',
-    icon: rubyrails,
-  },
-  {
     name: 'graphql',
     icon: graphql,
   },
@@ -124,63 +116,34 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'Front-End Developer',
-    company_name: 'Cover Hunt',
+    title: 'Computer Engineering',
+    company_name: 'UNEG',
     icon: coverhunt,
     iconBg: '#333333',
-    date: 'Aug 2021 - Feb 2022',
-  },
-  {
-    title: 'Mentor (Volunteer)',
-    company_name: 'Microverse',
-    icon: microverse,
-    iconBg: '#333333',
-    date: 'Mar 2022 - May 2022',
+    date: 'Nov 2015 - Feb 2022',
   },
   {
     title: 'Junior Software Engineer',
-    company_name: 'Kelhel',
+    company_name: 'Ofimuebles CA',
     icon: kelhel,
     iconBg: '#333333',
     date: 'May 2022 - Oct 2022',
   },
   {
     title: 'Full Stack Developer',
-    company_name: 'Diversity Cyber Council',
-    icon: dcc,
+    company_name: 'Aythem',
+    icon: microverse,
     iconBg: '#333333',
-    date: 'Sep 2022 - Present',
+    date: 'Mar 2024 - Present',
   },
 ];
 
 const projects = [
   {
-    id: 'project-1',
-    name: 'KomiKult',
-    description: 'A comic characters list app that displays Marvel characters.',
-    tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'mongodb',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
-      },
-    ],
-    image: komikult,
-    repo: 'https://github.com/shaqdeff/KomiKult',
-    demo: 'https://shaqdeff.github.io/KomiKult/',
-  },
-  {
     id: 'project-2',
-    name: 'Leaderboard',
+    name: 'Memus',
     description:
-      'A leaderboard list app that displays scores submitted by different players.',
+      'An application made in React where you can create notes and they are saved in a Mongodb database.',
     tags: [
       {
         name: 'react',
@@ -196,35 +159,13 @@ const projects = [
       },
     ],
     image: leaderboard,
-    repo: 'https://github.com/shaqdeff/Leaderboard',
-    demo: 'https://shaqdeff.github.io/Leaderboard/',
-  },
-  {
-    id: 'project-3',
-    name: 'Math Magicians',
-    description: 'This is a single-page calculator app built with React',
-    tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
-    ],
-    image: math,
-    repo: 'https://github.com/shaqdeff/Math-Magicians',
-    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+    repo: 'https://github.com/Jhonesp/Memus-app',
+    demo: 'https://jhonesp.github.io/Memus-app/',
   },
   {
     id: 'project-4',
-    name: 'Movie Metro',
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    name: '3d Model Rotator',
+    description: `Project made with React/vite using three.js and Mediapipe/tasks-vision to move a 3D model using hand detection`,
     tags: [
       {
         name: 'nextjs',
@@ -240,14 +181,13 @@ const projects = [
       },
     ],
     image: movie,
-    repo: 'https://github.com/shaqdeff/Movie-Metro',
-    demo: 'https://movie-metro.netlify.app/',
+    repo: 'https://github.com/Jhonesp/Rotador-3d-React-Mediapipe',
+    demo: 'https://jhonesp.github.io/Rotador-3d-React-Mediapipe/',
   },
   {
-    id: 'project-5',
-    name: 'Nyeusi Fest Site',
-    description:
-      'This is a demo concert website for a music festival called Nyeusi.',
+    id: 'project-3',
+    name: 'Aythen-lite',
+    description: 'Billing section for the Aythem company. Made with React, Redux. This demo is a part of the development I carried out, it has functional menus but it is not completely functional.',
     tags: [
       {
         name: 'nextjs',
@@ -262,9 +202,9 @@ const projects = [
         color: 'pink-text-gradient',
       },
     ],
-    image: nyeusi,
-    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
-    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+    image: math,
+    repo: 'https://github.com/Jhonesp/aythen_lite',
+    demo: 'https://jhonesp.github.io/aythen_lite/',
   },
 ];
 
